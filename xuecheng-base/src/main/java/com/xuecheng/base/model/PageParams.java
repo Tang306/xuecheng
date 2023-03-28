45b190be5fbf61a96d9201dfb6b945553c562f6e
Tangcw <tangcw0306@163.com>
2023-03-28 10:50:55 +0800

    设计接口

---
+++ xuecheng-base/src/main/java/com/xuecheng/base/model/PageParams.java
@@ -0,0 +1,30 @@
+package com.xuecheng.base.model;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @author tangcw
+ * @Desc 分页查询通用参数
+ * @date 2023/3/28 10:27
+ */
+@Data
+@ToString
+public class PageParams {
+
+    //当前页码
+    private Long pageNo = 1L;
+
+    //每页记录数默认值
+    private Long pageSize =10L;
+
+    public PageParams(){
+
+    }
+
+    public PageParams(long pageNo,long pageSize){
+        this.pageNo = pageNo;
+        this.pageSize = pageSize;
+    }
+
+}
