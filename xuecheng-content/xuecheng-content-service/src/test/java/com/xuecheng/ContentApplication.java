dff0f2610195d87d1179e85bc546c1e87b0672de
Tangcw <tangcw0306@163.com>
2023-03-28 11:26:22 +0800

    开发content持久层并测试

---
+++ xuecheng-content/xuecheng-content-service/src/test/java/com/xuecheng/ContentApplication.java
@@ -0,0 +1,16 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tangcw
+ * @Desc
+ * @date 2023/3/28 11:15
+ */
+@SpringBootApplication
+public class ContentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ContentApplication.class, args);
+    }
+}
