823d32e3f1d559c039e9efa1e1a5567e65b255fb
Tangcw <tangcw0306@163.com>
2023-03-31 16:57:12 +0800

    content-controller设置校验规则（分组校验）并完成接口测试

---
+++ xuecheng-base/src/main/java/com/xuecheng/base/execption/ValidationGroups.java
@@ -0,0 +1,12 @@
+package com.xuecheng.base.execption;
+
+/**
+ * @author tangcw
+ * @Desc 校验分组
+ * @date 2023/3/31 16:44
+ */
+public class ValidationGroups {
+    public interface Insert {};
+    public interface Update{};
+    public interface Delete{};
+}
--- xuecheng-content/xuecheng-content-model/src/main/java/com/xuecheng/content/model/dto/AddCourseDto.java
+++ xuecheng-content/xuecheng-content-model/src/main/java/com/xuecheng/content/model/dto/AddCourseDto.java
@@ -1,5 +1,6 @@
 package com.xuecheng.content.model.dto;
 
+import com.xuecheng.base.execption.ValidationGroups;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -16,26 +17,31 @@ import javax.validation.constraints.Size;
 @ApiModel(value = "AddCourseDto", description = "新增课程基本信息")
 public class AddCourseDto {
 
-    @NotEmpty(message = "课程名称不能为空")
+    @NotEmpty(groups = {ValidationGroups.Insert.class},message = "添加课程名称不能为空")
+    @NotEmpty(groups = {ValidationGroups.Update.class},message = "修改课程名称不能为空")
     @ApiModelProperty(value = "课程名称", required = true)
     private String name;
 
-    @NotEmpty(message = "适用人群不能为空")
+    @NotEmpty(groups = {ValidationGroups.Insert.class},message = "添加课程适用人群不能为空")
+    @NotEmpty(groups = {ValidationGroups.Update.class},message = "修改课程适用人群不能为空")
     @Size(message = "适用人群内容过少",min = 10)
     private String users;
 
     @ApiModelProperty(value = "课程标签")
     private String tags;
 
-    @NotEmpty(message = "课程分类不能为空")
+    @NotEmpty(groups = {ValidationGroups.Insert.class},message = "添加课程分类不能为空")
+    @NotEmpty(groups = {ValidationGroups.Update.class},message = "修改课程分类不能为空")
     @ApiModelProperty(value = "大分类", required = true)
     private String mt;
 
-    @NotEmpty(message = "课程分类不能为空")
+    @NotEmpty(groups = {ValidationGroups.Insert.class},message = "添加课程分类不能为空")
+    @NotEmpty(groups = {ValidationGroups.Update.class},message = "修改课程分类不能为空")
     @ApiModelProperty(value = "小分类", required = true)
     private String st;
 
-    @NotEmpty(message = "课程等级不能为空")
+    @NotEmpty(groups = {ValidationGroups.Insert.class},message = "添加课程等级不能为空")
+    @NotEmpty(groups = {ValidationGroups.Update.class},message = "修改课程等级不能为空")
     @ApiModelProperty(value = "课程等级", required = true)
     private String grade;
 
@@ -48,7 +54,8 @@ public class AddCourseDto {
     @ApiModelProperty(value = "课程图片", required = true)
     private String pic;
 
-    @NotEmpty(message = "收费规则不能为空")
+    @NotEmpty(groups = {ValidationGroups.Insert.class},message = "添加课程收费规则不能为空")
+    @NotEmpty(groups = {ValidationGroups.Update.class},message = "修改课程收费规则不能为空")
     @ApiModelProperty(value = "收费规则，对应数据字典", required = true)
     private String charge;
 
