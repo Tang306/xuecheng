d54ab5ea688e958b48a3bab27fd280a5dc2bbbba
Tangcw <tangcw0306@163.com>
2023-04-06 14:32:26 +0800

    创建网关模块xuecheng-gateway，通过访问网关接口实现对content数据进行查询

---
+++ xuecheng-gateway/src/main/java/com/xuecheng/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author tangcw
+ * @Desc
+ * @date 2023/4/6 14:14
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
